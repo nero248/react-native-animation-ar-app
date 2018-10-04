49c90f7ea986ecfaa258b377b5ca9ab2592036eb
ducnt <ducnt.248@gmail.com>
2018-10-04 15:15:57 +0900

    modified:   .gitignore modified:   App.js deleted:    README.md modified:   app.json modified:   package.json

--- App.js
+++ App.js
@@ -1,11 +1,21 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+import { Constants } from 'expo';
+
+// You can import from local files
+import AssetExample from './components/AssetExample';
+
+// or any pure javascript modules available in npm
+import { Card } from 'react-native-elements'; // 0.19.1
+
+import InitialARScene from './js/MainScene';
+//var InitialARScene = require('./js/MainScene.js');
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <InitialARScene />
       </View>
     );
   }
@@ -14,8 +24,10 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
-  },
+    paddingTop: Constants.statusBarHeight,
+    backgroundColor: '#ecf0f1',
+  }
+
 });

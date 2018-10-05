3c8af616ba653a7c2f32da7dd94a4efbfe200978
HoangVanTung <tunghv16@gmail.com>
2018-10-05 15:17:50 +0900

    modified:   App.js 	modified:   app.json 	modified:   package-lock.json 	modified:   package.json

--- App.js
+++ App.js
@@ -2,12 +2,6 @@ import * as React from 'react';
 import { Text, View, StyleSheet } from 'react-native';
 import { Constants } from 'expo';
 
-// You can import from local files
-import AssetExample from './components/AssetExample';
-
-// or any pure javascript modules available in npm
-import { Card } from 'react-native-elements'; // 0.19.1
-
 import InitialARScene from './js/MainScene';
 //var InitialARScene = require('./js/MainScene.js');
 
@@ -16,6 +10,7 @@ export default class App extends React.Component {
     return (
       <View style={styles.container}>
         <InitialARScene />
+        <Text>test</Text>
       </View>
     );
   }
